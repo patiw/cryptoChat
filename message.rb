9d81ecc4ef2165dee14d893901a40f78b0b2bdf1
illumitata <gothic8282@gmail.com>
2017-11-17 18:22:35 +0100

    start working on Message.rb module

--- message.rb
+++ message.rb
@@ -1,4 +1,16 @@
 #Message module
-  class Message
-
+module Message
+  def refreshTextBox(box)
+    stringText = File.read('message.txt')
+    box.setText(stringText)
   end
+
+  def sendTextBox(box, adres)
+    file = File.open(adres, 'a+')
+    file.puts(box.toPlainText())
+    box.clear
+    file.close
+  end
+
+  module_function :refreshTextBox, :sendTextBox
+end
