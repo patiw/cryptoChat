a45acf86c0b979d7ebbcac3331704a4b1b076369
wgrabowskii <wgrabowskii@wp.pl>
2017-11-18 11:40:41 +0100

    commit

--- cryptoChat.rb
+++ cryptoChat.rb
@@ -15,43 +15,43 @@ if $PROGRAM_NAME == __FILE__
 
     # key must be entered before the message process
     # suggestion: make the key generator and loading in other place
-    # a = XorshiftGen.new
-    # key = a.bytes(32).scan(/......../)
-    # key = key*" "
+    a = XorshiftGen.new
+    key = a.bytes(32).scan(/......../)
+    key = key*" "
 
-    # x = File.read('text').chomp
+    x = File.read('text').chomp
 
-    # loop do
-        # puts "Write something: (type qqqqq to exit)"
-    #    break if x == ""
+    loop do
+        #puts "Write something: (type qqqqq to exit)"
+        break if x == ""
 
-        # x = $stdin.gets.chomp
-    #    break if x == "qqqqq"
-    #    puts x
+        #x = $stdin.gets.chomp
+       break if x == "qqqqq"
+       puts x
 
-    #    puts "Encrypted text: "
+       puts "Encrypted text: "
         # make class with initial varaibles of message and key
-    #    message1 = Encryption.new(x, key)
+       message1 = Encryption.new(x, key)
         # x1 is the output of 3DES encrypt witch is an array
-    #    x1 = message1.tripledes_encrypt
+       x1 = message1.tripledes_encrypt
         # format the array to readable form
-    #    puts x1.blocks(8).to_text
+       puts x1.blocks(8).to_text
 
-    #    puts "After decryption: "
+       puts "After decryption: "
         # make class with initial varaibles of message and key
-    #    message2 = Encryption.new(x1, key)
+       message2 = Encryption.new(x1, key)
         # x2 is the output of 3DES decrypt witch is an array
-    #    x2 = message2.tripledes_decrypt
+       x2 = message2.tripledes_decrypt
         # format the array to readable form
-    #    puts x2#.recover_end_lines
+       puts x2 # .recover_end_lines
 
-    #    tmpArr = x.split("")
-    #    tmpArr.shift(8)
-    #    x = tmpArr.join
+       tmpArr = x.split("")
+       tmpArr.shift(8)
+       x = tmpArr.join
 
         ### TO DO: make x1 a string and encrypt the string, not an array
-    #    sleep(5)
-    # end
+       sleep(5)
+    end
 
   # QtApp patch for cryptoChat satisfy
   class QtApp < Qt::MainWindow
@@ -150,7 +150,6 @@ if $PROGRAM_NAME == __FILE__
       vbox1.addWidget contacts
       contacts.resize 150, 510
       contacts.move 390, 40
-
     end
 
     def about
@@ -164,7 +163,7 @@ if $PROGRAM_NAME == __FILE__
 
     def sendText
       @plik = File.open('message.txt', 'a+')
-      @plik.puts(@edit2.toPlainText())
+      @plik.puts(@edit2.toPlainText)
       @edit2.clear
       @plik.close
     end
